c24c580fb00059a86bb03fe0ef900f514d289615
Fabio Henrique Zultanski <49455033+fzultanski@users.noreply.github.com>
2025-03-13 22:02:51 +0000

    Correcao de valor mostrado errado na carta 2 e inclusao de densidade populacional e pib per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,12 @@ int main() {
     // Variaveis da carta 1
     char cd_estado_1, cd_carta_1[3], nm_cidade_1[25];
     int nr_populacao_1, nr_ponto_turistico_1;
-    float nr_area_1, vl_pib_1;
+    float nr_area_1, vl_pib_1, nr_densidade_1, vl_pib_per_capita_1;
 
     // Variaveis da carta 2
     char cd_estado_2, cd_carta_2[3], nm_cidade_2[25];
     int nr_populacao_2, nr_ponto_turistico_2;
-    float nr_area_2, vl_pib_2;
+    float nr_area_2, vl_pib_2, nr_densidade_2, vl_pib_per_capita_2;
 
     printf("BEM VINDO AO SUPER TRUNFO!\n\n");
     printf("Digite as Informações da CARTA 1:\n\n");
@@ -60,6 +60,14 @@ int main() {
     printf("Número do Pontos Turísticos:\n");
     scanf("%d", &nr_ponto_turistico_2);
 
+    // Calcula densidade populacional das cartas
+    nr_densidade_1 = (float) nr_area_1 / nr_populacao_1;
+    nr_densidade_2 = (float) nr_area_2 / nr_populacao_2;
+
+    // Calcula PIB per capita das cartas
+    vl_pib_per_capita_1 = (float) vl_pib_1 / nr_populacao_1;
+    vl_pib_per_capita_2 = (float) vl_pib_2 / nr_populacao_2;
+
     // Exibicao das informacoes da carta 1
     printf("\nCARTA 1\n");
     printf("Estado.....................: %c\n", cd_estado_1);
@@ -69,6 +77,8 @@ int main() {
     printf("Área.......................: %.2f km²\n", nr_area_1);
     printf("PIB........................: %.2f bilhões de reais\n", vl_pib_1);
     printf("Número de Pontos Turísticos: %d\n", nr_ponto_turistico_1);
+    printf("Densidade Populacional.....: %.2f hab/km²\n", nr_densidade_1);
+    printf("PIB per Capita.............: %.2f reais\n", vl_pib_per_capita_1);
 
     // Exibicao das informacoes da carta 2
     printf("\nCARTA 2\n");
@@ -76,9 +86,11 @@ int main() {
     printf("Código.....................: %s\n", cd_carta_2);
     printf("Nome da Cidade.............: %s\n", nm_cidade_2);
     printf("População..................: %d\n", nr_populacao_2);
-    printf("Área.......................: %.2f km²\n", nr_area_1);
-    printf("PIB........................: %.2f bilhões de reais\n", vl_pib_1);
-    printf("Número de Pontos Turísticos: %d\n\n", nr_ponto_turistico_2);
+    printf("Área.......................: %.2f km²\n", nr_area_2);
+    printf("PIB........................: %.2f bilhões de reais\n", vl_pib_2);
+    printf("Número de Pontos Turísticos: %d\n", nr_ponto_turistico_2);
+    printf("Densidade Populacional.....: %.2f hab/km²\n", nr_densidade_2);
+    printf("PIB per Capita.............: %.2f reais\n\n", vl_pib_per_capita_2);
 
     return 0;
 }
